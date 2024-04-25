840ace3d363295bacfa2242317a22ab0e85ebc5c
Indra Jatmiko <indrajatmiko@gmail.com>
2024-04-25 21:14:25 +0700

    fix faq 2

--- themes/dexignzone/ombe/resources/views/faq.blade.php
+++ themes/dexignzone/ombe/resources/views/faq.blade.php
@@ -4,22 +4,22 @@
 <!-- Main Content Start -->
 <main class="page-content space-top">
     <div class="container">
-        @foreach($faqs as $faq)
-        <div class="accordion dz-accordion style-2" id="accordionFaq{{$faq->id}}">
+        <div class="accordion dz-accordion style-2" id="accordionFaq">
+            @foreach($faqs as $faq)
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="heading{{$faq->id}}">
                         <a href="javascript:void(0);" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapse{{$faq->id}}" aria-expanded="true" aria-controls="collapse{{$faq->id}}">
                             {{ $faq->tanya }}
                         </a>
                     </h2>
-                    <div id="collapse{{$faq->id}}" class="accordion-collapse collapse {{ $faq->id == 1 ? 'show' : ''}}" aria-labelledby="heading{{$faq->id}}" data-bs-parent="#accordionFaq{{$faq->id}}">
+                    <div id="collapse{{$faq->id}}" class="accordion-collapse collapse {{ $faq->id == 1 ? 'show' : ''}}" aria-labelledby="heading{{$faq->id}}" data-bs-parent="#accordionFaq">
                         <div class="accordion-body">
                             <p class="m-b0">{{ $faq->jawab }}</p>
                         </div>
                     </div>
                 </div>
-            </div>
             @endforeach
+        </div>
     </div>
 </main>
 <!-- Main Content End -->
